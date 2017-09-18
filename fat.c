f10b030aaa4a7559d1b4f81336584f1827b06adf
Alex Salgado <alexsalgado1@gmail.com>
2017-09-18 19:10:53 -0300

    calcula o fatorial

---
+++ fat.c
@@ -0,0 +1,33 @@
+/*******************
+*
+* Nome: Alex
+* Calcula o Fatorial
+*
+**********************/
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    int i;
+    int fat;
+    
+    do{
+       printf("\nEntre com um numero:");
+       scanf("%d", &num);
+    }
+    while(num<=0);
+    
+    //calcular o fatorial;
+    fat = 1;
+    for(i=num; i>=1; i--)
+    {
+        fat = fat * i;
+    }
+    
+    printf("\nO fatorial = %d\n", fat);
+    
+    system("pause");
+    return 0;
+}
